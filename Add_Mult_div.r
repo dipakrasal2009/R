fbe2f7a0a1f6f67db9db2f1381dc47c3bdc3190c
dipakrasal2009 <dipakrasal2009@gmail.com>
2024-08-31 22:55:00 +0530

    in this file is use to addition of two numbers and also Division and substraction multiplication

---
+++ Add_Mult_div.r
@@ -0,0 +1,23 @@
+# Define two integer vectors of length 4
+vector1 <- c(4L, 8L, 12L, 16L)
+vector2 <- c(2L, 4L, 6L, 8L)
+
+# Addition of the vectors
+addition_result <- vector1 + vector2
+
+# Multiplication of the vectors
+multiplication_result <- vector1 * vector2
+
+# Division of the vectors
+division_result <- vector1 / vector2
+
+# Print the results
+print("Addition of vectors:")
+print(addition_result)
+
+print("Multiplication of vectors:")
+print(multiplication_result)
+
+print("Division of vectors:")
+print(division_result)
+
